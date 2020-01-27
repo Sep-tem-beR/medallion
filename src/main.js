3fe8f5e7410fd9d7215baa2a713051f7940e38a8
Еvgenij Pushkaryov <rosrys2013@mail.ru>
2020-01-27 14:05:25 +0300

    import owl, import fa, ini. owl and Email scripts

--- src/main.js
+++ src/main.js
@@ -1,3 +1,41 @@
-import './css/common.css'
-import './js/common.js'
+import './owl/owl.carousel.css'
+import './owl/owl.theme.default.css'
+import './js/owl.carousel'
+import './js/all.js'
 import './scss/main.scss'
+
+
+$( document ).ready(function() {
+    $(".madallions__owl-carousel, .trophy-medallions__owl-carousel, .thread-and-taxidermy__owl-carousel, .carved-home-decoration__owl-carousel, .carving-and-natural-materials__owl-carousel, .fixtures__owl-carousel").owlCarousel({
+        items: 1,
+        margin: 2,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 10000,
+        navigation : true,
+        singleItem : true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        nav: true,
+        navText: ['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>']
+    });
+
+   	//E-mail Ajax Send
+	$(".contact__form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
